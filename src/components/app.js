2fdb892e835fb15f2ee32c1260c15cb0ec63d2fc
Trevor Blades <tdblades@gmail.com>
2016-08-15 22:44:18 -0700

    Initial commit

--- src/components/app.js
+++ src/components/app.js
@@ -1,8 +1,58 @@
 var React = require('react');
 
+var members = [
+  {
+    name: 'Miguel Barbosa',
+    slug: 'miguel',
+    twitter: 'yeahfilms'
+  },
+  {
+    name: 'Trevor Blades',
+    slug: 'trevor',
+    twitter: 'trevorblades'
+  },
+  {
+    name: 'Marvin Sanchez',
+    slug: 'marvin',
+    twitter: 'marvinsanchez'
+  }
+];
+
 var App = React.createClass({
   render: function() {
-    return <h1>Hello, world!</h1>;
+    return (
+      <div className="rad-app">
+        <div className="rad-cover">
+          <div className="rad-cover-logo">
+            <img src="assets/logo.svg"/>
+          </div>
+        </div>
+        <div className="rad-content">
+          <div className="rad-members">
+            {members.map(function(member, index) {
+              return (
+                <div className="rad-member" key={index}>
+                  <img className="rad-member-profile"
+                      src={'assets/members/' + member.slug + '.jpg'}/>
+                  <h4>{member.name}</h4>
+                  <h5>
+                    <a href={'https://twitter.com/' + member.twitter}
+                        target="_blank">
+                      <img src="assets/twitter.svg"/>
+                      <span>{'@' + member.twitter}</span>
+                    </a>
+                  </h5>
+                </div>
+              );
+            })}
+          </div>
+          <div className="rad-content-wrap">
+            <h1>A gang of creatives</h1>
+            <p>Really Awesome Doings is a group of creators with diverse skill sets. We make awesome doings for a living and work on cool projects in our spare time.</p>
+          </div>
+        </div>
+      </div>
+    );
   }
 });
 
