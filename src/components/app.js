55a91d4f6bf68da28aa2a37803d83f646eb4644b
Trevor Blades <tdblades@gmail.com>
2016-08-16 00:02:14 -0700

    Move members to its own component

--- src/components/app.js
+++ src/components/app.js
@@ -1,22 +1,6 @@
 var React = require('react');
 
-var members = [
-  {
-    name: 'Miguel Barbosa',
-    slug: 'miguel',
-    twitter: 'yeahfilms'
-  },
-  {
-    name: 'Trevor Blades',
-    slug: 'trevor',
-    twitter: 'trevorblades'
-  },
-  {
-    name: 'Marvin Sanchez',
-    slug: 'marvin',
-    twitter: 'marvinsanchez'
-  }
-];
+var Members = require('./members');
 
 var App = React.createClass({
   render: function() {
@@ -32,24 +16,7 @@ var App = React.createClass({
             <h1>A gang of creatives</h1>
             <p>RAD is a group of creators with diverse skill sets. We make awesome doings for a living and work on cool projects in our spare time.</p>
           </div>
-          <div className="rad-members">
-            {members.map(function(member, index) {
-              return (
-                <div className="rad-member" key={index}>
-                  <img className="rad-member-profile"
-                      src={'assets/members/' + member.slug + '.jpg'}/>
-                  <h4>{member.name}</h4>
-                  <h5>
-                    <a href={'https://twitter.com/' + member.twitter}
-                        target="_blank">
-                      <img src="assets/twitter.svg"/>
-                      <span>{'@' + member.twitter}</span>
-                    </a>
-                  </h5>
-                </div>
-              );
-            })}
-          </div>
+          <Members/>
         </div>
       </div>
     );
