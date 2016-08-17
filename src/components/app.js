11db22dd42bbfba2904faf28b27038a81b16b960
Trevor Blades <tdblades@gmail.com>
2016-08-16 20:38:44 -0700

    Add app prefix to all classnames

--- src/components/app.js
+++ src/components/app.js
@@ -17,17 +17,17 @@ var App = React.createClass({
   render: function() {
     return (
       <div className="rad-app" onScroll={this._onScroll}>
-        <div className="rad-cover">
-          <h6 className="rad-cover-scroll"
+        <div className="rad-app-cover">
+          <h6 className="rad-app-cover-scroll"
               style={{opacity: 1 - this.state.scrollPosition / (window.innerHeight / 2)}}>
             Scroll down
           </h6>
-          <div className="rad-cover-logo">
+          <div className="rad-app-cover-logo">
             <img src="assets/logo.svg"/>
           </div>
         </div>
-        <div className="rad-content">
-          <div className="rad-content-wrap">
+        <div className="rad-app-content">
+          <div className="rad-app-content-wrap">
             <h1>A gang of creatives</h1>
             <p>RAD is a group of creators with diverse skill sets. We make awesome doings for a living and work on cool projects in our spare time.</p>
           </div>
